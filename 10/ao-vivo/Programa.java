9caa6465cea1e67226d03d1771fd59a7452166ff
Willian Sant Anna <wsantanna@digitalhouse.com>
2023-04-18 12:58:44 -0300

    Publicação do código fonte comentado da Aula 10

--- 10/ao-vivo/Programa.java
+++ 10/ao-vivo/Programa.java
@@ -1,9 +1,14 @@
-public class Programa {
+class Programa {
     public static void main(String[] args) {
         Tabuada tabuada = new Tabuada();
-        String valor = System.console().readLine("Digite o valor que deseja tabuada: ");
+
+        // Algoritmo 1) Capturo os valores do usuário 
+        String valor = System.console().readLine("Digite o valor que deseja tabuada.");
+
+        // Algoritmo 2) Calculo a tabuada 
         String resultado = tabuada.calcular(valor);
 
+        // Algoritmo 3) Apresento o resultado no terminal 
         System.out.println(resultado);
     }
 }
--- 10/ao-vivo/Tabuada.java
+++ 10/ao-vivo/Tabuada.java
@@ -1,13 +1,24 @@
-public class Tabuada {
+// 1) Classe = objeto
+class Tabuada {
+
+    // 2) Atributo = variáveis
+    public int limiteDoMultiplicador = 10;
+    
+    // 3) Método = função(parametro)
     public String calcular(String valor) {
         
+        // 1) Pede o valor para o usuário...
+        // String valor = System.console().readLine("Digite o valor que deseja tabuada.");
         String tabuada = "";
-
-        for(int multiplicador = 0; multiplicador <= 10; multiplicador++) {
+        
+        // 2) calcula a tabuada...
+        for(int multiplicador = 0; multiplicador <= limiteDoMultiplicador; multiplicador++) {
             int resultado = Integer.parseInt(valor) * multiplicador;
+            // 3) Apresentar o resultado no terminal
+            // System.out.println(valor + "x" + multiplicador + "=" + resultado);
             tabuada += valor + "x" + multiplicador + "=" + resultado + "\n";
         }
 
         return tabuada;
-    }
+    }    
 }
