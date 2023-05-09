36308edc9620dbafd402cda178999f3d213fa38f
Willian Sant Anna <wsantanna@digitalhouse.com>
2023-05-09 13:50:58 -0300

    Implementação do código fonte da Aula 16

---
+++ 16/ao-vivo/IQuadrado.java
@@ -0,0 +1,13 @@
+public interface IQuadrado {
+
+    public String tipo = "Quadrado";
+    
+    public double getLargura();
+
+    public void setLargura(double largura);
+
+    public double getAltura();
+
+    public void setAltura(double altura);
+
+}
---
+++ 16/ao-vivo/ITriangulo.java
@@ -0,0 +1,13 @@
+public interface ITriangulo {
+
+    public String tipo = "Triangulo";
+    
+    public double getBase();
+
+    public void setBase(double base);
+
+    public double getAltura();
+
+    public void setAltura(double altura);
+
+}
